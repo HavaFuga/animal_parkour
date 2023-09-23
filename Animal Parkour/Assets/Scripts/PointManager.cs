182f77faf975db0ba580815a84331802db54c0f0
HavaFuga <hava.fuga@students.fhnw.ch>
2023-09-23 15:30:51 +0200

    feat: add collectable points with movement

--- Animal Parkour/Assets/Scripts/PointManager.cs
+++ Animal Parkour/Assets/Scripts/PointManager.cs
@@ -8,18 +8,21 @@ public class PointManager : MonoBehaviour
     public float frequency = 0.5f;
     public Vector3 posOrigin = new Vector3();
     public Vector3 tempPos = new Vector3();
+    public Vector3 rotation = new Vector3(0, 30f, 0);
     
     // Start is called before the first frame update
     void Start()
     {
-        // posOrigin = transform.position;
+        posOrigin = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // tempPos = posOrigin;
-        // tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
-        // transform.position = tempPos;
+        tempPos = posOrigin;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        transform.position = tempPos;
+        transform.Rotate(rotation * Time.deltaTime);
+
     }
 }
